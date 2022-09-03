380d3ec59851d3c29ec6149eb164b7a6f2baf4d4
NataliaMelissa <50978032+NataliaMelissa@users.noreply.github.com>
2022-09-02 19:44:24 -0500

    Delete Code directory

--- Code/Carga_Archivo.R
+++
@@ -1,4 +0,0 @@
-#Carga de Datos
-DFHotel_Ruido <- read.csv("hotel_bookings_miss.csv", header = TRUE, stringsAsFactors = FALSE)
-View(DFHotel_Ruido)
-save(DFHotel_Ruido, file = "~/R/EA-Admin-Info/Data/DF_Ruido.RData")
